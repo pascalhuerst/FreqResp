32409799e0840c0cd0c49e00671058d1231481c6
Pascal Huerst <pascal.huerst@gmail.com>
2017-02-01 10:24:35 +0100

    Fixed main, to include to buildroot

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <cmath>
 #include <algorithm>
+#include <numeric>
 
 #include "Device.h"
 
@@ -26,22 +27,8 @@ double rms(const std::vector<double>& samples)
 		rms += sample * sample;
 		rms = sqrt(rms / samples.size());
 	}
+	rms = sqrt(rms / samples.size());
 	return rms;
-}
-
-void saveBuffer(const std::vector<double>& s, const std::string& fileName)
-{
-	std::ofstream outfile(fileName, std::ofstream::out);
-
-	if (!outfile.is_open()) {
-		std::cout << "File not opened!" << std::endl;
-	}
-
-	IntIndexer<double>::setStartIndex(0);
-	std::ostream_iterator<IntIndexer<double>> iter(outfile, "\n");
-	std::copy(s.begin(), s.end(), iter);
-	outfile.flush();
-	outfile.close();
 }
 
 // create logarithmically well distributed measuring points,
@@ -53,7 +40,8 @@ void createMeasuringPoints(std::vector<double>& points, int pointsPerDecade, dou
 	double k = std::pow(10, 1.0 / e);
 
 	for (int exp=0; exp<e; exp++) {
-		double currentVal = round(pow(k, exp) * 10.0) / 10.0;
+		//double currentVal = round(pow(k, exp) * 10.0) / 10.0;
+		double currentVal = (pow(k, exp) * 10.0) / 10.0;
 		points.push_back(currentVal);
 
 		double tmpMax = maxHz*10.0;
@@ -71,6 +59,21 @@ void createMeasuringPoints(std::vector<double>& points, int pointsPerDecade, dou
 
 	points.erase(iter, points.end());
 	std::sort(points.begin(), points.end());
+}
+
+void saveBuffer(const std::vector<double>& s, const std::string& fileName)
+{
+	std::ofstream outfile(fileName, std::ofstream::out);
+
+	if (!outfile.is_open()) {
+		std::cout << "File not opened!" << std::endl;
+	}
+
+	IntIndexer<double>::setStartIndex(0);
+	std::ostream_iterator<IntIndexer<double>> iter(outfile, "\n");
+	std::copy(s.begin(), s.end(), iter);
+	outfile.flush();
+	outfile.close();
 }
 
 void run(SharedTerminateFlag terminateRequest)
@@ -121,10 +124,8 @@ void run(SharedTerminateFlag terminateRequest)
 
 			std::vector<double> keys(samples.size());
 			std::iota(std::begin(keys), std::end(keys), 0);
-			w->updateSineGraph(keys, samples);
 
 			*currentFreqResp = dBuForVolts(rms(samples));
-			w->updateFreqGraph(points, freqResp);
 
 			std::cout << "[" << std::distance(points.begin(), currentFrequency)
 					  << "] dBu @ " << double(*currentFrequency)
@@ -147,23 +148,6 @@ void run(SharedTerminateFlag terminateRequest)
 int main(int argc, char *argv[])
 {
 
-	auto devs = Device::getDevices();
-	if (devs.empty()) {
-		std::cout << "No devices Found!" << std::endl;
-		exit(0);
-	}
-
-	std::cout << "Found " << devs.size() << " devices. Opening " <<
-				 devs.front().id << " [" << devs.front().ver << "]" << std::endl;
-
-
-	// Create frequency Map with measuring frequencies
-	std::vector<double>points;
-	double fMin = 20;
-	double fMax = 20000;
-	int pointsPerDecade = 100;
-	createMeasuringPoints(points, pointsPerDecade, fMin, fMax);
-
 #if 0
 	// Plot points
 	std::ostream_iterator<double> iter(std::cout, "\n");
@@ -181,60 +165,17 @@ int main(int argc, char *argv[])
 	outfile.close();
 #endif
 
+	auto terminate = createTerminateFlag();
+	std::thread t1(run, terminate);
 
-	try {
-		Device dev(devs.front());
-		std::cout << "Version: " << dev.version() << std::endl;
+	while(getchar() != 'q') {
 
-		auto terminate = createTerminateFlag();
-		SharedSampleStorage samples(new std::vector<std::vector<double>>(points.size()));
 
-		std::thread t1(readOneBuffer, &dev, points, samples, terminate);
 
-		while (!terminate->load()) {
-
-			//std::cout << "InputStatus:  " << dev.analogInputStatus() << std::endl;
-			//std::cout << "OutputStatus: " << dev.analogOutputStatus() << std::endl;
-			for (auto i = samples->begin(); i!= samples->end(); ++i) {
-				if (!i->empty())
-					std::cout << "Samples [" << std::distance(samples->begin(),i) << "] :" << i->size() << std::endl;
-			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		}
-
-		terminate->store(true);
-		t1.join();
-
-		auto s = samples->begin();
-		auto p = points.begin();
-		char buf[3];
-		std::vector<PrintablePair<double,double>> freqResp;
-
-		for (unsigned i = 0; s != samples->end(); s++, p++, i++) {
-			sprintf(buf, "%03i", i);
-			saveBuffer(*s, "sampledFreq_" + std::string(buf) + "_" + std::to_string(*p));
-
-			// Creates a vector containing frequency response values as ( first=freq | second=RMS )
-			freqResp.push_back(
-						// Find max and divide by sqrt(2) to get RMS
-						//PrintablePair<double,double>(*p,dBuForVolts(*max_element(s->begin(), s->end()) / sqrt(2))));
-						// calculate RMS from samples
-						PrintablePair<double,double>(*p, dBuForVolts(rms(*s))));
-
-			std::ofstream outfile("resp.txt", std::ofstream::out);
-
-			if (!outfile.is_open()) {
-				std::cout << "File not opened!" << std::endl;
-			}
-			std::ostream_iterator<PrintablePair<double,double>> iter(outfile, "\n");
-			std::copy(freqResp.begin(), freqResp.end(), iter);
-		}
-
-	} catch (DeviceException e) {
-		std::cout << "e.what(): " << e.what() << std::endl;
-	} catch (std::exception e) {
-		std::cout << "e.what(): " << e.what() << std::endl;
 	}
+
+	terminate->store(true);
+	t1.join();
 
 	return 0;
 }
